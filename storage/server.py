33ae4838325f69c9e22b74f599d915dcf5358099
IsaacVention <isaac.mills@vention.cc>
2025-07-24 13:06:53 -0400

    Remove component discovery, provide strong typing instead

--- storage/server.py
+++ storage/server.py
@@ -1,17 +1,35 @@
 from fastapi import FastAPI
-from orchestrator import storage
 from fastapi.responses import FileResponse
+from orchestrator.accessor import ModelAccessor
+from orchestrator.storage import bootstrap
 
+from components.gripper.models import Gripper
+from components.scanner.models import Scanner
+
+
+# --------- BOOTSTRAP APP ---------
 app = FastAPI()
-storage.bootstrap(app)
 
+grippers = ModelAccessor(Gripper)
+scanners = ModelAccessor(Scanner)
+
+bootstrap(
+    app,
+    models=[
+        ("gripper", Gripper, grippers),
+        ("scanner", Scanner, scanners),
+    ],
+)
+
+# --------- ROUTES ---------
 @app.get("/")
 def serve_index():
     return FileResponse("./static/index.html")
 
+
 @app.get("/gripper-center/{id}")
 def get_center(id: int):
-    gripper = storage.gripper.get(id)
+    gripper = grippers.get(id)
     if not gripper:
         return {"error": "Gripper not found"}
     return {"center": gripper.get_center_of_mass()}
